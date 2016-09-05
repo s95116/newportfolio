eb3e947ac44d4fc98725c3cfc97fa7bd1b80cffe
Seth Keel <seth.keel@gmail.com>
2016-09-04 20:43:31 -0400

    setting up file structure

---
+++ server.js
@@ -0,0 +1,38 @@
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var exphbs = require('express-handlebars');
+
+var app = express();
+
+
+var port = process.env.PORT || 3309;
+
+
+
+
+app.use(express.static(process.cwd() + '/public')); 
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(methodOverride('_method'));  
+
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+
+app.set('view engine', 'handlebars');
+
+//***********Routes to Handlebars**********//
+
+
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+
+//*************************************************//
